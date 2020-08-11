41bdca459ec65b4090dae6128c0a020c8d7fb6ed
justynalezuch <justynalezuch@gmail.com>
2020-08-11 17:55:34 +0200

    Dropdown menu - balance period

--- app.js
+++ app.js
@@ -12,4 +12,17 @@ $(document).ready(function () {
         currentMonth='0'+currentMonth;
 
     $('input#date').val(`${currentYear}-${currentMonth}-${currentDay}`);
+
+    // --- Financial balance period
+
+    $('a#custom').click(function () {
+        $('#date-range').toggle();
+    });
+    $('html').click(function() {
+        $('#date-range').hide();
+    });
+    $('a#custom, #date-range').click(function(e){
+        e.stopPropagation();
+    });
 });
+
