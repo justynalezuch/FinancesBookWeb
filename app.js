879c675729bae7ac67439fa3500c6eba2376ca57
justynalezuch <justynalezuch@gmail.com>
2020-08-12 20:32:36 +0200

    Summary and pie chart

--- app.js
+++ app.js
@@ -24,5 +24,39 @@ $(document).ready(function () {
     // $('a#custom, #date-range, input[type=date]').click(function(e){
     //     e.stopPropagation();
     // });
+
+    // Load google charts
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Kategoria', 'Wydatek'],
+            ['Jedzenie', 540.12],
+            ['Mieszkanie', 125.12],
+            ['Transport', 65.12],
+            ['Telekomunikacja', 63.12],
+            ['Opieka zdrowotna', 55.12],
+            ['Ubranie', 52.12],
+            ['Higiena', 45.12],
+            ['Dzieci', 54.12],
+            ['Rozrywka', 54.12],
+            ['Wycieczka', 54.12],
+            ['Szkolenia', 54.12],
+            ['Książki', 54.12],
+            ['Oszczędności', 54.12],
+            ['Na złotą jesień, czyli emeryturę', 54.12],
+            ['Spłata długów', 54.12],
+            ['Darowizna', 54.12],
+            ['Inne wydatki', 54.12]
+        ]);
+
+        // Optional; add a title and set the width and height of the chart
+        var options = {'title':'Wydatki', 'width':550, 'height':400};
+
+        // Display the chart inside the <div> element with id="piechart"
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+        chart.draw(data, options);
+    }
 });
 
